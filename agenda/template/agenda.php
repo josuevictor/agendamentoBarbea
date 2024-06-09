19f3632d6227341ed06ce238477306732507984c
josuevictor <victorarroxellas01@gmail.com>
2024-06-09 18:13:21 -0300

    template agenda iniciado

--- agenda/template/agenda.php
+++ agenda/template/agenda.php
@@ -1,4 +1,27 @@
 <?php include_once("./header.php")?>
-  <h1>teste agenda</h1>
-  <i class="fas fa-eye"></i>
+  <div class="container">
+    <h1 id="main-title">Agenda</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Nome</th>
+          <th scope="col">Contato</th>
+          <th scope="col">Servico</th>
+          <th scope="col">Data_hora</th>
+          <th scope="col">Status_servico</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>cliente_teste</td>
+          <td>82999999999</td>
+          <td>Corte de cabelo</td>
+          <td>10/06/2024 20:00</td>
+          <td>agendado</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
  <?php include_once("./footer.php")?>
